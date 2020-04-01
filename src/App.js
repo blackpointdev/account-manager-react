67d4c19cdb0e32d56996e63941191396deafa9df
Marcin Wiśnios <marcinw1998@gmail.com>
2020-04-01 18:53:42 +0200

    Initial commit.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,64 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import axios from 'axios'
+import Table from 'react-bootstrap/Table';
+import Button from 'react-bootstrap/Button';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            paginatedOperations: []
+        }
+    }
+
+    componentDidMount() {
+        axios.get("http://localhost:8080/api/operations").then((response) => {
+            this.setState({
+                paginatedOperations: response.data
+            })
+        });
+
+        // fetch("http://localhost:8080/api/operations")
+        //     .then(response => response.json())
+        //     .then(response => {
+        //         this.state.paginatedOperations = response;
+        //     })
+    };
+
+    render() {
+        let operations = this.state.paginatedOperations.map((paginableContent) => {
+            return (
+                <tr key={operation.id}>
+                    <td>{operation.id}</td>
+                    <td>{operation.name}</td>
+                    <td>TODO</td>
+                    <td>{operation.balance}</td>
+                    <td>
+                        <Button variant="outline-success" size="sm" className="mr-2">Edit</Button>
+                        <Button variant="outline-danger" size="sm">Delete</Button>
+                    </td>
+                </tr>
+            )
+        });
+        return (
+            <div className="App container">
+                <Table>
+                    <thead>
+                        <tr>
+                            <th>#</th>
+                            <th>Name</th>
+                            <th>User</th>
+                            <th>Balance</th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {operations}
+                    </tbody>
+                </Table>
+            </div>
+        );
+    }
 }
 
 export default App;
